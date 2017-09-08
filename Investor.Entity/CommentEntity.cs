c6367d0bd16f894f52aa3ea1212f3dbbea9f7c1b
ira-vitalii <ira-vitalii@outlook.com>
2017-09-08 10:32:11 +0300

    Data Base create

---
+++ Investor.Entity/CommentEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Entity
+{
+    class CommentEntity
+    {
+        [Key]
+        public int CommentId { set; get; }
+
+        public string Text { set; get; }
+        public bool Pubished { set; get; }
+
+        public DateTime PublishedOn { set; get; }
+        public DateTime CreatedOn { set; get; }
+
+        public int UserId { set; get; }
+        public UserEntity User { set; get; }
+
+        public int PostId { set; get; }
+        public PostEntity Post { set; get; }
+    }
+}
---
+++ Investor.Entity/TagEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Entity
+{
+    class TagEntity
+    {
+        public int TagId { set; get; }
+        public string Name { set; get; }
+        public string Url { set; get; }
+ 
+        public IList <PostEntity> Posts { set; get; }
+        public TagEntity()
+        {
+            Posts = new List<PostEntity>();
+        }
+    }
+}
