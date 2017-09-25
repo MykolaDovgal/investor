8382b49e5c3f23b4d85f2f409987c1bc67d448f8
ira-vitalii <ira-vitalii@outlook.com>
2017-09-25 17:25:42 +0300

    added currency converter

---
+++ Investor.Web/wwwroot/js/currency_converter.js
@@ -0,0 +1,26 @@
+﻿window.onload = function () {
+	$.ajax({
+		url: "http://devel.farebookings.com/api/curconversor/EUR/UAH/1/",
+		dataType: 'jsonp',
+		success: function (data) {
+			console.log(data.UAH);
+			console.log($('#EUR').text());
+			$('#EUR').text($('#EUR').text() + data.UAH);
+		}
+
+	}); 
+	$.ajax({
+		url: "http://devel.farebookings.com/api/curconversor/USD/UAH/1/",
+		dataType: 'jsonp',
+		success: function (data) {
+			console.log(data.UAH);
+			console.log($('#USD').text());
+			$('#USD').text($('#USD').text() + data.UAH);
+		}
+
+	});
+}
+
+
+
+	
