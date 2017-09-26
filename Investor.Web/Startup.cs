7035a28325b54c44ff105f1e13e569d7dbd04665
ira-vitalii <ira-vitalii@outlook.com>
2017-09-26 13:54:22 +0300

    Path for facebookComments merge

--- Investor.Web/Startup.cs
+++ Investor.Web/Startup.cs
@@ -34,7 +34,7 @@ namespace Investor.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<NewsContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("TestConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("GearConnection")));
             // Add framework services.
 
             services.AddTransient<ICategoryRepository, CategoryRepository>();
