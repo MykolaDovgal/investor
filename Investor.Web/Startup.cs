4363d3a337a1f8bd31fb9041cb3ab1be0fd38cbd
Nazarii Beseniuk <prorok97ok@gmail.com>
2017-09-27 18:29:55 +0300

    added authorization, cahange Identity

--- Investor.Web/Startup.cs
+++ Investor.Web/Startup.cs
@@ -13,6 +13,8 @@ using AutoMapper;
 using Investor.Service;
 using Investor.Repository.Interfaces;
 using Investor.Service.Interfaces;
+using Investor.Entity;
+using Microsoft.AspNetCore.Identity;
 
 namespace Investor.Web
 {
@@ -34,8 +36,20 @@ namespace Investor.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<NewsContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("TestConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("GearConnection")));
             // Add framework services.
+
+            services.AddIdentity<UserEntity, IdentityRole>(opts =>
+            {
+                opts.Password.RequiredLength = 6;   
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false; 
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<NewsContext>()
+            .AddDefaultTokenProviders();
+
 
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             //services.AddTransient<ITagRepository, TagRepository>();
@@ -77,6 +91,8 @@ namespace Investor.Web
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseAuthentication();
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
@@ -84,7 +100,7 @@ namespace Investor.Web
                 routes.MapRoute("post", "post/{id}",
                     defaults: new { controller = "Post", action = "Index" });
 
-                routes.MapRoute("blog","category/{url}",
+                routes.MapRoute("blog", "category/{url}",
                     defaults: new { controller = "Category", action = "Index" });
 
                 routes.MapRoute(
