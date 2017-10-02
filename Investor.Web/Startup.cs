84639fb7acf18e523bd619fb975635585c7a108a
ira-vitalii <ira-vitalii@outlook.com>
2017-10-02 16:09:49 +0300

    admin view

--- Investor.Web/Startup.cs
+++ Investor.Web/Startup.cs
@@ -81,6 +81,7 @@ namespace Investor.Web
 
             app.UseMvc(routes =>
             {
+                
                 routes.MapRoute("post", "post/{id}",
                     defaults: new { controller = "Post", action = "Index" });
 
@@ -90,6 +91,10 @@ namespace Investor.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areas",
+                template: "{area:exists}/{controller=Home}/{action=Index}");
             });
         }
     }
