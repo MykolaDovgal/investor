e78d0394f9b214ae15c3a63e8b36bcde73909608
ira-vitalii <ira-vitalii@outlook.com>
2017-10-09 16:28:38 +0300

    add tags typeahead

--- Investor.Web/wwwroot/js/admin/content/new-post.js
+++ Investor.Web/wwwroot/js/admin/content/new-post.js
@@ -1,19 +1,40 @@
-﻿
+﻿let initTypeahead = function () {
+	var tags = [];
 
+	$(document).ready(function () {
 
+		tinymce.init({
+			mode: "specific_textareas",
+			selector: ".text-editor",
+			theme: "modern",
+			plugins: [
+				"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+				"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+				"save table contextmenu directionality emoticons template paste textcolor"
+			],
 
-let initTypeahead = function () {
+			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
+			style_formats: [
+				{ title: 'Bold text', inline: 'b' },
+				{ title: 'Red text', inline: 'span', styles: { color: '#ff0000' } },
+				{ title: 'Red header', block: 'h1', styles: { color: '#ff0000' } },
+				{ title: 'Example 1', inline: 'span', classes: 'example1' },
+				{ title: 'Example 2', inline: 'span', classes: 'example2' },
+				{ title: 'Table styles' },
+				{ title: 'Table row 1', selector: 'tr', classes: 'tablerow1' }
+			]
+		});
 
-	var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-		'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-		'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-		'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-		'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-		'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-		'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-		'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-		'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-	];
+		$.ajax({
+			url: "/api/Content/GetAllTags",
+			type: "GET",
+			success: function (data) {
+				data.data.map(item => {
+					tags.push(item.name);
+				});
+			}
+		});
+	});
 
 	var substringMatcher = function (strs) {
 		return function findMatches(q, cb) {
@@ -38,34 +59,12 @@ let initTypeahead = function () {
 
 	$("#tagTypeahead").tagsinput({
 		typeaheadjs: {
-			name: 'states',
-			source: substringMatcher(states)
+			name: 'tags',
+			source: substringMatcher(tags)
 		}
 	});
 
-	$(document).ready(function () {
-		tinymce.init({
-			mode: "specific_textareas",
-			selector: ".text-editor",
-			theme: "modern",
-			plugins: [
-				"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-				"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-				"save table contextmenu directionality emoticons template paste textcolor"
-			],
-
-			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
-			style_formats: [
-				{ title: 'Bold text', inline: 'b' },
-				{ title: 'Red text', inline: 'span', styles: { color: '#ff0000' } },
-				{ title: 'Red header', block: 'h1', styles: { color: '#ff0000' } },
-				{ title: 'Example 1', inline: 'span', classes: 'example1' },
-				{ title: 'Example 2', inline: 'span', classes: 'example2' },
-				{ title: 'Table styles' },
-				{ title: 'Table row 1', selector: 'tr', classes: 'tablerow1' }
-			]
-		});
-	});
+	
 }
 
 
