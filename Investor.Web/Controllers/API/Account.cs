059c08a1faf624e897cf80ddbf91022c05eb10dc
ira-vitalii <ira-vitalii@outlook.com>
2017-12-20 14:40:57 +0200

    admin login page fixed

---
+++ Investor.Web/Controllers/API/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Investor.Model;
+using Investor.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Investor.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class Account : Controller
+    {
+        private readonly IUserService _userService;
+        public Account(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [Route("nicknames")]
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetNicknames()
+        {
+            var users = await  _userService.GetAllUsersAsync<TableUserPreview>();
+            return users.Select(c => c.UserName);
+        }
+        [Route("emails")]
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetEmails()
+        {
+            var users = await _userService.GetAllUsersAsync<TableUserPreview>();
+            return users.Select(c => c.Email);
+        }
+    }
+}
