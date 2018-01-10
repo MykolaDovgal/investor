295b39662889edbe27235c12a90fbae3d3f580fb
Nazarii Beseniuk <prorok97ok@gmail.com>
2018-01-10 13:45:08 +0200

    Added caching of posts views count

--- Investor.Service/StatisticsService.cs
+++ Investor.Service/StatisticsService.cs
@@ -7,24 +7,35 @@ using Investor.Entity;
 using Investor.Model;
 using Investor.Repository.Interfaces;
 using Investor.Service.Interfaces;
+using Investor.Service.Utils;
 
 namespace Investor.Service
 {
     public class StatisticsService : IStatisticsService
     {
         private readonly IStatisticsRepository _statisticsRepository;
-        public StatisticsService(IStatisticsRepository statisticsRepository)
+        private readonly CacheService _cacheService;
+        public StatisticsService(IStatisticsRepository statisticsRepository, CacheService cacheService)
         {
             _statisticsRepository = statisticsRepository;
+            _cacheService = cacheService;
         }
         public async Task AddStatisticsAsync(Statistics statistics)
         {
             await _statisticsRepository.AddStatistics(Mapper.Map<Statistics, StatisticsEntity>(statistics));
         }
 
-        public async Task<int> GetPostViewsCountByIdAsync(int postId,bool isUnique = false)
+        public async Task<int> GetPostViewsCountByIdAsync(int postId, bool isUnique = false)
         {
-            return await _statisticsRepository.GetPostViewsCountByIdAsync(postId, isUnique);
+            var key = $"post_{postId}_views_count";
+
+            int? postViewsCount = (int?)_cacheService.GetValue(key);
+
+            if (postViewsCount.HasValue) return postViewsCount.Value;
+
+            postViewsCount = await _statisticsRepository.GetPostViewsCountByIdAsync(postId, isUnique);
+            _cacheService.SetValue(key, postViewsCount.Value, TimeSpan.FromMinutes(15));
+            return postViewsCount.Value;
         }
     }
 }
