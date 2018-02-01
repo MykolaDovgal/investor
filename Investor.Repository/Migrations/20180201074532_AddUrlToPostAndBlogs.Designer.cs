092907676ed739233722662fd4fc630d8892945a
Nazarii Beseniuk <prorok97ok@gmail.com>
2018-02-01 09:47:36 +0200

    Added db migration

--- Investor.Repository/Migrations/20180201074532_AddUrlToPostAndBlogs.Designer.cs
+++ Investor.Repository/Migrations/20180201074532_AddUrlToPostAndBlogs.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Investor.Repository.Migrations
 {
     [DbContext(typeof(NewsContext))]
-    partial class NewsContextModelSnapshot : ModelSnapshot
+    [Migration("20180201074532_AddUrlToPostAndBlogs")]
+    partial class AddUrlToPostAndBlogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Investor.Repository/Migrations/20180201074532_AddUrlToPostAndBlogs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Investor.Repository.Migrations
+{
+    public partial class AddUrlToPostAndBlogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Posts");
+        }
+    }
+}
